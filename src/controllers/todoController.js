ae8a5a25ae13ba9abed0f02d866c4d2b0706dbb2
Titus <titusouko@gmail.com>
2025-10-01 12:28:01 +0300

    Add get single todo by ID

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,7 +1,7 @@
-const todo = require('../models/todoModel');
+const Todo = require('../models/todoModel');
 
 //GET all todos
-exports.getAllTodos =  async () => {
+exports.getAllTodos =  async (req, res) => {
     try{
         const todos = await Todo.find().sort({createdAt: -1}); //Newest first
         res.status(200).json({
@@ -19,3 +19,32 @@ exports.getAllTodos =  async () => {
 
     }
 };
+
+//GET single todo by ID
+exports.getTodoById = async (req, res) =>{
+    try {
+        const todo = await Todo.findById(req.params.id);
+
+        if(!todo){
+            return res.status(404).json({
+                succes : false,
+                message : 'Todo not found'
+            });
+        }
+
+        res.status(200).json({
+            succes : true,
+            data : todo
+        });
+        
+    } catch (error) {
+
+        res.status(500).json({
+            succes : false,
+            message : 'Server Error',
+            error : error.message
+        });
+        
+    }
+};
+
